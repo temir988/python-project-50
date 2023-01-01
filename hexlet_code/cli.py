86cf551ea236b9da5baaa0b14283e1097f142421
Temirlan <bamblebee988@gmail.com>
2023-01-02 02:14:52 +0600

    [step 1]: basic project setup

---
+++ hexlet_code/cli.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def init():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
+
---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,10 @@
+from hexlet_code.cli import init
+
+
+def main():
+    print('Hello!')
+    init()
+
+
+if __name__ == '__main__':
+    main()
